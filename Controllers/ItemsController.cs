421ce7268b6018d00479efb8cc02f37956088bb7
Will Murchison <wmurch@hotmail.com>
2019-06-19 13:14:24 -0400

    added, get SKU, delete, and update

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -7,12 +7,18 @@ namespace wills_inventory.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ItemsController
+    public class ItemsController : ControllerBase
     {
+        private DatabaseContext db;
+
+        public ItemsController()
+        {
+            this.db = new DatabaseContext();
+        }
         [HttpPost]
         public ActionResult<Item> Post([FromBody]Item postItem)
         {
-            var db = new DatabaseContext();
+            //var db = new DatabaseContext();
             db.Items.Add(postItem);
             db.SaveChanges();
             return postItem;
@@ -20,19 +26,56 @@ namespace wills_inventory.Controllers
         [HttpGet]
         public ActionResult<List<Item>> Get()
         {
-            var db = new DatabaseContext();
+            // var db = new DatabaseContext();
             var results = db.Items;
             return results.ToList();
         }
-        [HttpGet("{Id}")]
+        [HttpGet("id/{Id}")]
         public ActionResult<Item> Get(int Id)
         {
-            var db = new DatabaseContext();
+            //var db = new DatabaseContext();
             var results = db.Items.FirstOrDefault(w => w.Id == Id);
 
             return results;
         }
+        [HttpGet("OutOfStock")]
+        public ActionResult<List<Item>> GetOutOfStock()
+        {
+            //var db = new DatabaseContext();
+            var results = db.Items.Where(w => w.NumberInStock == 0);
 
+            return results.ToList();
+        }
+        [HttpGet("SKU/{SKU}")]
+        public ActionResult<Item> GetSKU(int SKU)
+        {
+            //var db = new DatabaseContext();
+            var results = db.Items.FirstOrDefault(w => w.SKU == SKU);
 
+            return results;
+        }
+        [HttpPut("{Id}")]
+        public ActionResult<Item> Update(int Id, [FromBody]Item item)
+        {
+            //var db = new DatabaseContext();
+            var results = db.Items.FirstOrDefault(w => w.Id == Id);
+            results.SKU = item.SKU;
+            results.Name = item.Name;
+            results.NumberInStock = item.NumberInStock;
+            results.ShortDescription = item.ShortDescription;
+            results.Price = item.Price;
+            results.DateOrdered = item.DateOrdered;
+            db.SaveChanges();
+            return results;
+        }
+        [HttpDelete("{Id}")]
+        public ActionResult<Item> Delete(int Id)
+        {
+            //var db = new DatabaseContext();
+            var results = db.Items.FirstOrDefault(w => w.Id == Id);
+            db.Items.Remove(results);
+            db.SaveChanges();
+            return Ok();
+        }
     }
 }
